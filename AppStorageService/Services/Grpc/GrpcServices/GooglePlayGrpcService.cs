7c54b417b9be0689edb7de295bc8ef37fc4f9e54
Anton Kravchenko <78639975+AAxelx@users.noreply.github.com>
2025-03-04 02:15:51 +0200

    AT-2: Added services, gRPC clients, proto files and registered them

---
+++ AppStorageService/Services/Grpc/GrpcServices/GooglePlayGrpcService.cs
@@ -0,0 +1,28 @@
+using AppStorageService.Services.Grpc.GrpcServices.Abstractions;
+using AppStorageService.Services.Models;
+using AutoMapper;
+using Grpc.Core;
+
+namespace AppStorageService.Services.Grpc.GrpcServices;
+
+public class GooglePlayGrpcService(GooglePlayGrpc.GooglePlayService.GooglePlayServiceClient client, IMapper mapper) : IGooglePlayGrpcService
+{
+  public async Task SendApplicationAsync(ApplicationModel model)
+  {
+    try
+    {
+      var response = await client.RegisterApplicationAsync(mapper.Map<GooglePlayGrpc.ApplicationRequest>(model));
+
+      if (!response.Success)
+      {
+        throw new ApplicationException($"gRPC Error: {response.Message}");
+      }
+    }
+    catch (RpcException ex)
+    {
+      string errorMessage = $"gRPC request failed. StatusCode: {ex.StatusCode}, Detail: {ex.Status.Detail}";
+
+      throw new Exception(errorMessage, ex);
+    }
+  }
+}
